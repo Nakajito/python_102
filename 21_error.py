1abda57df3a4613cfbf5de661aa1f3d0f5ea9d82
Daniel Bonilla <87622543+Nakajito@users.noreply.github.com>
2023-04-02 18:47:06 +0000

    file

---
+++ 21_error.py
@@ -0,0 +1,7 @@
+print('Inicio')
+
+age = 17
+if age < 18:
+    raise Exception('No se permiten menores de edad') #raise Exception('No se permiten menores de edad')
+    # Exception: No se permiten menores de edad
+print('Fin')
---
+++ 22_error.py
@@ -0,0 +1,20 @@
+print('Inicio')
+try:
+    print(0/0)
+    
+    assert 1 != 1, 'Uno es diferente'
+    
+    age = 10
+    if age < 18:
+        raise Exception('No se permiten menores de edad')
+
+except ZeroDivisionError as error:
+    print(error)
+
+except AssertionError as error:
+    print(error)
+
+except Exception as error:
+    print(error)
+
+print('fin')
